0ca60bef2e27f1458c31605003146de76213c72d
Luis felipe rincon <lfrinconv@uqvirtual.edu.co>
2022-08-13 18:54:47 -0500

    Se anade el fondo blur a la interfaz del detalle

--- lib/presentation/movies/detail/detail_movie.dart
+++ lib/presentation/movies/detail/detail_movie.dart
@@ -9,6 +9,7 @@ import 'package:prueba_leal/presentation/movies/episode/detail_episode_page.dart
 import 'package:prueba_leal/presentation/shared/style/color.dart';
 import 'package:prueba_leal/presentation/shared/style/text_style.dart';
 import 'package:prueba_leal/presentation/shared/widgets/fille_button.dart';
+import 'package:prueba_leal/presentation/shared/widgets/image_blur.dart';
 import 'package:prueba_leal/presentation/shared/widgets/movie_image_card.dart';
 
 ///Pantalla que muesta con mas detalle la imagen de la serie
@@ -48,112 +49,123 @@ class _DetailMoviePageState extends State<DetailMoviePage> {
     return Scaffold(
       backgroundColor: LocalColor.negro,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 12),
-              GestureDetector(
-                onTap: () => Navigator.pop(context),
-                child: Row(
+        child: Stack(
+          children: [
+            Positioned(
+                child: ImageBlurWidget(
+                    imageUrl:
+                        "https://image.tmdb.org/t/p/w500${widget.movies[currentIndex].posterPath}")),
+            Positioned.fill(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Icon(
-                      Icons.chevron_left,
-                      color: LocalColor.blanco,
-                      size: 32,
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Text(widget.tittle,
-                        style: const TextStyle(
+                    const SizedBox(height: 12),
+                    GestureDetector(
+                      onTap: () => Navigator.pop(context),
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.chevron_left,
                             color: LocalColor.blanco,
-                            fontFamily: LocalTextStyle.fontFamilyGilroy,
-                            fontWeight: LocalTextStyle.fontWeightRegular,
-                            fontSize: 16)),
+                            size: 32,
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Text(widget.tittle,
+                              style: const TextStyle(
+                                  color: LocalColor.blanco,
+                                  fontFamily: LocalTextStyle.fontFamilyGilroy,
+                                  fontWeight: LocalTextStyle.fontWeightRegular,
+                                  fontSize: 16)),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CarouselSlider.builder(
+                            carouselController: carouselController,
+                            itemCount: widget.movies.length,
+                            options: CarouselOptions(
+                              onPageChanged: ((index, reason) {
+                                setState(() {
+                                  currentIndex = index;
+                                });
+                              }),
+                              height: MediaQuery.of(context).size.height * 0.40,
+                              aspectRatio: 16 / 9,
+                              viewportFraction: 0.67,
+                              initialPage: 0,
+                              enlargeCenterPage: true,
+                              scrollDirection: Axis.horizontal,
+                            ),
+                            itemBuilder: (ctx, index, real) {
+                              return CardMoviewImage(
+                                  pathImage:
+                                      "w400${widget.movies[index].posterPath}");
+                            },
+                          ),
+                          const SizedBox(height: 24),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.43,
+                            child: AutoSizeText(
+                              widget.movies[currentIndex].name,
+                              textAlign: TextAlign.center,
+                              maxLines: 3,
+                              style: const TextStyle(
+                                  color: LocalColor.blanco,
+                                  fontFamily: LocalTextStyle.fontFamilyGilroy,
+                                  fontWeight: LocalTextStyle.fontWeightBold,
+                                  fontSize: 24),
+                            ),
+                          ),
+                          const SizedBox(height: 24),
+                          RatingBarIndicator(
+                            rating:
+                                (widget.movies[currentIndex].voteAverage / 2) -
+                                    0.45,
+                            itemBuilder: (context, index) => const Icon(
+                              Icons.star,
+                              color: LocalColor.gris,
+                            ),
+                            itemCount: 5,
+                            itemSize: 15.0,
+                            direction: Axis.horizontal,
+                          ),
+                          const SizedBox(height: 15),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 16),
+                            child: AutoSizeText(
+                              "IMDb: ${widget.movies[currentIndex].voteAverage}",
+                              maxLines: 1,
+                              style: const TextStyle(
+                                  color: LocalColor.gris,
+                                  fontSize: 12,
+                                  fontFamily: LocalTextStyle.fontFamilyGilroy),
+                              textAlign: TextAlign.start,
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.43,
+                              child: FilledButton(
+                                  text: "Wacth now",
+                                  onPressed: () {
+                                    goToEpisodes(widget.movies[currentIndex]);
+                                  }))
+                        ],
+                      ),
+                    )
                   ],
                 ),
               ),
-              const SizedBox(height: 24),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CarouselSlider.builder(
-                      carouselController: carouselController,
-                      itemCount: widget.movies.length,
-                      options: CarouselOptions(
-                        onPageChanged: ((index, reason) {
-                          setState(() {
-                            currentIndex = index;
-                          });
-                        }),
-                        height: MediaQuery.of(context).size.height * 0.40,
-                        aspectRatio: 16 / 9,
-                        viewportFraction: 0.67,
-                        initialPage: 0,
-                        enlargeCenterPage: true,
-                        scrollDirection: Axis.horizontal,
-                      ),
-                      itemBuilder: (ctx, index, real) {
-                        return CardMoviewImage(
-                            pathImage:
-                                "w400${widget.movies[index].posterPath}");
-                      },
-                    ),
-                    const SizedBox(height: 24),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.43,
-                      child: AutoSizeText(
-                        widget.movies[currentIndex].name,
-                        textAlign: TextAlign.center,
-                        maxLines: 3,
-                        style: const TextStyle(
-                            color: LocalColor.blanco,
-                            fontFamily: LocalTextStyle.fontFamilyGilroy,
-                            fontWeight: LocalTextStyle.fontWeightBold,
-                            fontSize: 24),
-                      ),
-                    ),
-                    const SizedBox(height: 24),
-                    RatingBarIndicator(
-                      rating:
-                          (widget.movies[currentIndex].voteAverage / 2) - 0.45,
-                      itemBuilder: (context, index) => const Icon(
-                        Icons.star,
-                        color: LocalColor.gris,
-                      ),
-                      itemCount: 5,
-                      itemSize: 15.0,
-                      direction: Axis.horizontal,
-                    ),
-                    const SizedBox(height: 15),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 16),
-                      child: AutoSizeText(
-                        "IMDb: ${widget.movies[currentIndex].voteAverage}",
-                        maxLines: 1,
-                        style: const TextStyle(
-                            color: LocalColor.gris,
-                            fontSize: 12,
-                            fontFamily: LocalTextStyle.fontFamilyGilroy),
-                        textAlign: TextAlign.start,
-                      ),
-                    ),
-                    const SizedBox(height: 15),
-                    SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.43,
-                        child: FilledButton(
-                            text: "Wacth now",
-                            onPressed: () {
-                              goToEpisodes(widget.movies[currentIndex]);
-                            }))
-                  ],
-                ),
-              )
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
