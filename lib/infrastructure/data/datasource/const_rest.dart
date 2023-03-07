6a1fed803db090ee0def53ed31ff2c1f42cadea3
FelipeR1ncon <89667616+FelipeR1ncon@users.noreply.github.com>
2023-03-06 22:24:38 -0500

    fix data source

--- lib/infrastructure/data/datasource/const_rest.dart
+++ lib/infrastructure/data/datasource/const_rest.dart
@@ -1,8 +1,6 @@
 class ConstRest {
-  ///WARNING ! ESTO CLARAMENTE ES UNA MALA PRACTICA DEBERIA DE ESTAR
-  ///EN UN SERVIDOR SEGURO O FIRESTORE, SE REALIZA DE ESTA MANERA POR
-  ///CUESTIONES DE TIEMPO, pongo sss para disumular aunque de poco sirve.
-  static const sss = "8980390b40b3a776eb05dd067977e723";
+
+  static const sss = "";
 
   ////API MOVIES/////
   static const baseUrl = "https://api.themoviedb.org/3/tv/";
